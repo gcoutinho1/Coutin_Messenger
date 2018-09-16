5b8cb81c2ced4e4ab976854c7f7bb8bb1d0c214c
Guilherme Coutinho <guilhermecoutinho7777@gmail.com>
2018-09-16 00:29:21 -0300

    projeto configurado com firebase

--- app/src/main/java/coutinhodeveloper/com/coutinmessenger/MainActivity.java
+++ app/src/main/java/coutinhodeveloper/com/coutinmessenger/MainActivity.java
@@ -3,15 +3,24 @@ package coutinhodeveloper.com.coutinmessenger;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.client.Firebase;
+
 /** Created by Guilherme Coutinho
  *  on 15/09/2018
  */
 
 public class MainActivity extends AppCompatActivity {
 
+    private Firebase firebase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Firebase.setAndroidContext(this);
+        firebase = new Firebase("https://coutinmessenger.firebaseio.com/mensagens");
+        firebase.setValue("teste do firebase");
+
     }
 }
