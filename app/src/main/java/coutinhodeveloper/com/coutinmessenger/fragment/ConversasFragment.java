ec1739de64f3d52ddec64d140a70b78535577a7d
Guilherme Coutinho <guilhermecoutinho7777@gmail.com>
2018-10-01 12:41:21 -0300

    fragments & tabs

---
+++ app/src/main/java/coutinhodeveloper/com/coutinmessenger/fragment/ConversasFragment.java
@@ -0,0 +1,35 @@
+package coutinhodeveloper.com.coutinmessenger.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import coutinhodeveloper.com.coutinmessenger.R;
+
+
+/** Created by Guilherme Coutinho
+ *  on 01/10/2018
+ */
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ConversasFragment extends Fragment {
+
+
+    public ConversasFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_conversas, container, false);
+    }
+
+}
